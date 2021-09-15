6712d2693a0df1a91be7e33d049e4b3b49894764
RobsDuarte <robsduar@gmail.com>
2021-09-14 23:11:13 -0300

    comment removed

--- Model/Connection.php
+++ Model/Connection.php
@@ -5,8 +5,7 @@ require_once "Model\Config.php";
 try{
     $connection = new PDO("mysql:host=".DB_SERVER.";dbname:".DB_USERNAME."",DB_USERNAME,DB_PASSWORD);
     // set the PDO error mode to exception
-    $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "Connected successfully"; 
+    $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);    
 }catch(PDOException $e){
     echo "Connection failed: " . $e->getMessage();
 }
