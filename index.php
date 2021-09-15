8526e76c3a7e536b46bcd7ab8f120d893145c44d
RobsDuarte <robsduar@gmail.com>
2021-09-14 23:10:57 -0300

    index done

--- index.php
+++ index.php
@@ -1,1 +1,18 @@
 <?php
+
+require __DIR__."/vendor/autoload.php";
+
+use Source\Http\Router;
+use Source\Utils\View;
+
+define('URL','http://localhost/projetos/AppointmentSystem');
+
+View::init([
+    'URL' => URL
+]);
+
+$objectRouter = new Router(URL);
+
+include __DIR__.'/Source/Routes/RouteCollection.php';
+
+echo $objectRouter->run()->sendResponse();
