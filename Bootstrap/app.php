1c836360462a5364211727f0e894f2fe1bce611f
RobsDuarte <robsduar@gmail.com>
2021-09-19 17:07:19 -0300

    file to load composer and environmet variables

---
+++ Bootstrap/app.php
@@ -0,0 +1,7 @@
+<?php
+
+//Carrega o autoload do composer
+require __DIR__."/../vendor/autoload.php";
+
+//Carrega ad variaveis de ambiente do projeto
+\Source\Common\Environment::load(__DIR__.'/../');
