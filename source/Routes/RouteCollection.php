df5fc6cff750907d22afa57064c552f82672869d
RobsDuarte <robsduar@gmail.com>
2021-09-20 13:02:28 -0300

    home route added

--- source/Routes/RouteCollection.php
+++ source/Routes/RouteCollection.php
@@ -1,11 +1,19 @@
 <?php
 
 use Source\Controller\Login;
+use Source\Controller\Home;
 use Source\Http\Response;
 
 $objectRouter->get('/',[
     function()
     {
         return new Response(200,Login::getLogin());
+    }
+]);
+
+$objectRouter->post('/home',[
+    function()
+    {
+        return new Response(200,Home::getHome());
     }
 ]);
