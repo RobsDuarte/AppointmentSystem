42c85e065405f5ef24a3a214f0ede483b0d0cd5f
RobsDuarte <robsduar@gmail.com>
2021-10-06 13:59:02 -0300

    conexão com DB

--- DB/Connection.php
+++ DB/Connection.php
@@ -1,9 +1,9 @@
 <?php
 
 try{
-    $connection = new PDO("mysql:host=".getenv('DB_SERVER').";dbname:".getenv('DB_USERNAME')."",getenv('DB_USERNAME'),getenv('DB_PASSWORD'));
+    $connection = new PDO("mysql:host=".getenv('DB_SERVER').";dbname:".getenv('DB_NAME')."",getenv('DB_USERNAME'),getenv('DB_PASSWORD'));
     // set the PDO error mode to exception
-    $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);    
+    $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);      
 }catch(PDOException $e){
     echo "Connection failed: " . $e->getMessage();
 }
