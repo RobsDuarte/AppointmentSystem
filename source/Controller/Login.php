8bbf674cb7f66639cd354e6543b5dfbce9c0eed1
RobsDuarte <robsduar@gmail.com>
2021-10-06 13:59:32 -0300

    controlador da pagina login(index)

--- source/Controller/Login.php
+++ source/Controller/Login.php
@@ -3,11 +3,33 @@
 namespace Source\Controller;
 
 use Source\Utils\View;
+use Source\Model\Data;
 
+session_start();
 class Login
 {
-    public static function getLogin()
+    private $model;
+    private $msg;
+
+    public static function getLogin($msg = [])
+    {    
+        return View::render('login',$msg);        
+    }
+
+    public static function requestLogin($request)
+    {      
+        $PostVars = $request['request']->getPostVars(); 
+        $_SESSION['name'] = $PostVars['nome'];              
+        return Data::getLoginFromBD($PostVars['nome']) ? View::render('home',['USER'=>$_SESSION['name']]) : self::getLogin(['ERROR' => self::ErrorMsg()]);
+    }
+
+    private static function ErrorMsg()
     {
-        return View::render('login');        
+        $msg = '<script type="text/javascript">
+                document.getElementById("ErrorMsg").innerHTML = "Login ou Senha incorretos";
+                document.getElementById("ErrorMsg").style.display = "block";
+                </script>';
+        return $msg;
     }
 }
+
